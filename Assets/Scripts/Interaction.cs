6f2e65221ea90c12eadfa1ac94dd66e8f880e1b1
Devin <devinpquinn4@gmail.com>
2023-10-03 18:44:16 -0400

    basic interaction

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class Interaction : MonoBehaviour
+{
+    public List<DialogText> dialogs;
+    private int dialogIndex = 0;
+    private int lineIndex = 0;
+
+    private GameObject dialogueCard;
+    private TextMeshProUGUI dialogueText;
+
+    [System.Serializable]
+    public class DialogText
+    {
+        [TextArea]
+        public List<string> lines; //the text blocks that make up this event
+
+        public DialogText()
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (PlayerController.instance.state == PlayerController.playerState.Exploring)
+            {
+                Interact();
+            }
+        }
+    }
+
+    private void Start()
+    {
+        dialogueCard = PlayerController.instance.dialogueCard;
+        dialogueText = PlayerController.instance.dialogueText;
+    }
+
+    public void Interact()
+    {
+        if(dialogIndex < dialogs.Count)
+        {
+            //lock player
+            PlayerController.instance.state = PlayerController.playerState.Interacting;
+            PlayerController.instance.interaction = this;
+
+            //start playing text
+            dialogueCard.SetActive(true);
+            lineIndex = 0;
+            dialogueText.SetText(dialogs[dialogIndex].lines[lineIndex]);
+        }
+    }
+
+    public void Advance()
+    {
+        lineIndex++;
+
+        //get next line of dialogue, check for events or end of interaction
+        if (dialogs[dialogIndex].lines.Count > lineIndex)
+        {
+            //continue dialogue
+            dialogueText.SetText(dialogs[dialogIndex].lines[lineIndex]);
+        }
+        else
+        {
+            //end dialogue
+            dialogIndex++;
+            dialogueCard.SetActive(false);
+            PlayerController.instance.state = PlayerController.playerState.Exploring;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     #region Variables
 
+    //state
     public enum playerState { Exploring, Interacting, Locked };
 
     public playerState state;
 
+    //movement
     private Rigidbody2D rb;
     public float moveSpeed = 3f;
     Vector2 movement;
 
+    //interaction
+    public GameObject dialogueCard;
+    public TextMeshProUGUI dialogueText;
+    public Interaction interaction;
+
+    //singleton
     private static PlayerController _player;
-    public static PlayerController Instance { get { return _player; } }
+    public static PlayerController instance { get { return _player; } }
 
     #endregion
 
@@ -43,6 +52,15 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(state == playerState.Interacting)
+        {
+            if ((Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space)) && interaction != null)
+            {
+                //advance interaction
+                interaction.Advance();
+            }
+        }
     }
 
     void FixedUpdate()
