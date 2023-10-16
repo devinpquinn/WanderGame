377c5170526fecd290c642c4780bb1f9f4d37072
Devin <devinpquinn4@gmail.com>
2023-10-16 00:24:24 -0400

    footsteps

--- Assets/Scripts/FootstepSounds.cs
+++ Assets/Scripts/FootstepSounds.cs
@@ -4,44 +4,20 @@ using UnityEngine;
 
 public class FootstepSounds : MonoBehaviour
 {
-    public AudioSource footstepSource;
+    private AudioSource footstepSource;
 
-    public List<AudioClip> dirtSounds;
     public List<AudioClip> grassSounds;
-    public List<AudioClip> stoneSounds;
-    public List<AudioClip> woodSounds;
 
     private int lastIndex;
 
-    public void PlayFootstepSound(string stepMaterial)
+    private void Awake()
     {
-        switch (stepMaterial)
-        {
-            
-            case "Dirt":
-                footstepSource.PlayOneShot(GetDirtSound());
-                break;
-            case "Grass":
-                footstepSource.PlayOneShot(GetGrassSound());
-                break;
-            case "Stone":
-                footstepSource.PlayOneShot(GetStoneSound());
-                break;
-            case "Wood":
-                footstepSource.PlayOneShot(GetWoodSound());
-                break;
-        }
+        footstepSource = GetComponent<AudioSource>();
     }
 
-    public AudioClip GetDirtSound()
+    public void PlayFootstepSound()
     {
-        int key = Random.Range(0, dirtSounds.Count);
-        while (key == lastIndex)
-        {
-            key = Random.Range(0, dirtSounds.Count);
-        }
-        lastIndex = key;
-        return dirtSounds[key];
+        footstepSource.PlayOneShot(GetGrassSound());
     }
 
     public AudioClip GetGrassSound()
@@ -53,27 +29,5 @@ public class FootstepSounds : MonoBehaviour
         }
         lastIndex = key;
         return grassSounds[key];
-    }
-
-    public AudioClip GetStoneSound()
-    {
-        int key = Random.Range(0, stoneSounds.Count);
-        while (key == lastIndex)
-        {
-            key = Random.Range(0, stoneSounds.Count);
-        }
-        lastIndex = key;
-        return stoneSounds[key];
-    }
-
-    public AudioClip GetWoodSound()
-    {
-        int key = Random.Range(0, woodSounds.Count);
-        while (key == lastIndex)
-        {
-            key = Random.Range(0, woodSounds.Count);
-        }
-        lastIndex = key;
-        return woodSounds[key];
     }
 }
