8b1fbcb1bb99c10b557fa8c18d803ad2f47650b8
Devin <devinpquinn4@gmail.com>
2023-12-16 14:15:14 -0500

    bizarro room

---
+++ Assets/Scripts/Event-Specific/BizarroHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BizarroHandler : MonoBehaviour
+{
+    private GameObject player;
+
+    private void OnEnable()
+    {
+        player = GameObject.Find("Player");
+        player.GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("Persistent Environment/Blockers/Perimeter").SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+
+    private void OnDisable()
+    {
+        player.GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.Find("Persistent Environment/Blockers/Perimeter").SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        player.GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.Find("Persistent Environment/Blockers/Perimeter").SetActive(true);
+    }
+}
