17561cb88d9ff0ff154ff781ad9acd0522fb43d1
Devin <devinpquinn4@gmail.com>
2024-05-25 15:04:52 -0400

    promo work

--- Assets/Scripts/ChopsHandler.cs
+++ Assets/Scripts/ChopsHandler.cs
@@ -4,24 +4,10 @@ using UnityEngine;
 
 public class ChopsHandler : MonoBehaviour
 {
-    public GameObject chopper;
-    public GameObject chopped;
-
     private void Start()
     {
         BlockerManager.SetupBlockers("NES");
         RoomManager.instance.currentRoom.GetComponent<Room>().doors = "NES";
-
-        //determine if this is the first or second clearing
-        if(PlayerPrefs.HasKey("Data_Chopping") && PlayerPrefs.GetInt("Data_Chopping") != GetComponent<Room>().id)
-        {
-            chopped.SetActive(true);
-        }
-        else
-        {
-            PlayerPrefs.SetInt("Data_Chopping", GetComponent<Room>().id);
-            chopper.SetActive(true);
-        }
     }
 
     private void OnEnable()
--- Assets/Scripts/Event-Specific/ChoppedHandler.cs
+++ Assets/Scripts/Event-Specific/ChoppedHandler.cs
@@ -4,27 +4,12 @@ using UnityEngine;
 
 public class ChoppedHandler : MonoBehaviour
 {
-    public List<AudioClip> ChopSounds;
-    private int lastIndex = -1;
-    private AudioSource src;
 
     public Transform bloodOrigin;
     public GameObject ps;
 
-    private void Awake()
-    {
-        src = GetComponent<AudioSource>();
-    }
-
     public void ChoppedSound()
     {
-        int index = Random.Range(0, ChopSounds.Count);
-        while (index == lastIndex)
-        {
-            index = Random.Range(0, ChopSounds.Count);
-        }
-        src.PlayOneShot(ChopSounds[index]);
-
         Instantiate(ps, bloodOrigin.transform.position, Quaternion.identity, bloodOrigin);
     }
 }
