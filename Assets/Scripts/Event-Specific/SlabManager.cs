62abe5d7c28a219164b38de4390ad86e39e77828
Devin <devinpquinn4@gmail.com>
2024-06-12 19:31:55 -0400

    staggering slab boys

--- Assets/Scripts/Event-Specific/SlabManager.cs
+++ Assets/Scripts/Event-Specific/SlabManager.cs
@@ -4,6 +4,39 @@ using UnityEngine;
 
 public class SlabManager : MonoBehaviour
 {
+    public List<GameObject> guys;
+
+    private void Start()
+    {
+        //check if we're returning to this room
+        if (PlayerPrefs.HasKey("Data_Slab"))
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Data_Slab", 1);
+
+            StartCoroutine(StaggerGuys());
+        }
+    }
+
+    IEnumerator StaggerGuys()
+    {
+        foreach (GameObject b in guys)
+        {
+            b.SetActive(false);
+        }
+
+        int index = 0;
+        while (index < guys.Count)
+        {
+            yield return new WaitForSeconds(Random.Range(0, 0.25f));
+            guys[index].SetActive(true);
+            index++;
+        }
+    }
+
     private void OnEnable()
     {
         PlayerController.instance.transform.Find("PlayerReflection").gameObject.SetActive(true);
